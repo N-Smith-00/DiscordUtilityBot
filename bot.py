359ba367f595d5ac2693b9fb8d133adcc22b5d57
Nicholas-Smith463 <Nicholas.Smith463@gmail.com>
2024-06-26 02:38:25 -0400

    add a slash command

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os, discord
+from discord import app_commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,20 +7,28 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True
-
+# needed for message event (not nessecary yet)
+# intents.message_content = True
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id=1048911119584084018))
     print(f"We have logged in as {client.user}")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.lower().startswith('$hello'):
-        await message.channel.send("Hello!")
+# regular message event (not nessecary yet)
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+@tree.command(
+    name="hello",
+    description="replies with hello",
+    guild=discord.Object(id=1048911119584084018)
+)
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hello {interaction.user.display_name}")
 
 client.run(TOKEN)
